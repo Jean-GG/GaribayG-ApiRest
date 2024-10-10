e24169ebb96eb7587c3f1ec9f935f11b7f36938e
Jean-GG <jcgaribayg02@hotmail.com>
2024-10-10 09:40:51 -0500

    JSDoc

---
+++ Parcial2/JSDoc/README.md
@@ -0,0 +1,3 @@
+# Documentación del Módulo Suma
+
+Este proyecto contiene un módulo JavaScript que proporciona una función para sumar dos números. La documentación se genera utilizando JSDoc.
---
+++ Parcial2/JSDoc/funcion.js
@@ -0,0 +1,21 @@
+/**
+ * Módulo que proporciona una función para sumar dos números.
+ * @module suma
+ */
+
+/**
+ * Suma dos números.
+ *
+ * @param {number} a - El primer número.
+ * @param {number} b - El segundo número.
+ * @returns {number} La suma de los dos números.
+ *
+ * @example
+ * // Ejemplo de uso:
+ * import { sumar } from './suma.js';
+ * const resultado = sumar(5, 10);
+ * console.log(resultado); // 15
+ */
+export function sumar(a, b) {
+    return a + b;
+}

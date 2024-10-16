b8d51bb2d6be812847b25db91b6349e91526c06f
Jean-GG <jcgaribayg02@hotmail.com>
2024-10-16 08:29:30 -0500

    Validacion

--- Parcial2/ValidadorExpress/server.js
+++ Parcial2/ValidadorExpress/server.js
@@ -1,20 +1,45 @@
 const express = require('express')
-//const cors = require('cors');
-
 const app = express()
-
-//app.use(cors())
-
 const port = 3001
+const { body, validationResult } = require('express-validator');
 
-const { validationResult } = require('express-validator'); 
-const  validadores = require('./validacion.js');      
-app.use(express.json()); 
-app.post("/usuario",validadores.validaUsuario(),(req,res)=>{ 
-const result = validationResult(req); 
+
+app.use(express.json());// Middleware para parsear JSON en el body de las solicitudes
+
+app.post('/registro', 
+  // Validacion
+  [
+    body('username')
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage('El nombre de usuario debe tener al menos 3 caracteres')
+      .escape(),
+    body('email')
+      .isEmail()
+      .withMessage('Debe proporcionar un correo electrónico válido')
+      .normalizeEmail(),
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('La contraseña debe tener al menos 6 caracteres')
+      .matches(/\d/)
+      .withMessage('La contraseña debe contener al menos un número')
+  ],
+  (req, res) => {
+    const errors = validationResult(req);// Aqui guarda los mensajes de error
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });// Si hay errores se devuelve un mensaje de error
+    }
+
+    res.send('Datos validados correctamente');// Si no hay errores seguira su curso normal
+  }
+);
+
+app.listen(port, () => {
+  console.log('Servidor iniciado en http://localhost:3001');
 });
 
 
-app.listen(port,() => {
-    console.log('La aplicacion esta en linea');
-})
+
+
+
